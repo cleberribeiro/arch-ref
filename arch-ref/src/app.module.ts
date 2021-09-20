daae1312b4b03e4e355e608a611e645d95960be7
Cleber Ribeiro <cleber.ramos.ribeiro@gmail.com>
2021-09-20 16:43:27 -0300

    refactor: add typeorm module and remove unnecessary imports

--- arch-ref/src/app.module.ts
+++ arch-ref/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeormConfig } from './infrastructure/configs/typeormconfig';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot(typeormConfig),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
