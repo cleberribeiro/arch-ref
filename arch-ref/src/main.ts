952342baf107e0a82a646b4ea0c606af811e222a
Cleber Ribeiro <cleber.ramos.ribeiro@gmail.com>
2021-10-03 17:45:37 -0300

    feat: add configuration ton Swagger

--- arch-ref/src/main.ts
+++ arch-ref/src/main.ts
@@ -1,8 +1,21 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Arch-Ref')
+    .setDescription('The Arch-Ref API reference to new projects')
+    .setVersion('1.0')
+    .addTag('arch-ref')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-doc', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
