2368e99a6162779d04244eeafffb99f37f58dae5
Cleber Ribeiro <cleber.ramos.ribeiro@gmail.com>
2021-10-04 22:35:23 -0300

    refactor: remove import unused

--- arch-ref/src/infrastructure/repository/dummy.repository.ts
+++ arch-ref/src/infrastructure/repository/dummy.repository.ts
@@ -3,7 +3,6 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { DummyEntity } from 'src/domain/entities/dummy/dummy.entity';
 import { Dummy, DummyCreate } from 'src/domain/protocols/dummy/dummy.interface';
 import { MongoRepository } from 'typeorm';
-import { databaseMapper } from './mappers/database.mapper';
 
 export class DummyRepository {
   constructor(
