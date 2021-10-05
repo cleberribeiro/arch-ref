8a2749c48679928ce7933867239c1cafad02144b
Cleber Ribeiro <cleber.ramos.ribeiro@gmail.com>
2021-10-04 22:35:45 -0300

    chore: add controller to findAll dummies

--- arch-ref/src/interface/http/controllers/dummy/dummy.controller.ts
+++ arch-ref/src/interface/http/controllers/dummy/dummy.controller.ts
@@ -1,12 +1,14 @@
-import { Body, Controller, InternalServerErrorException, Logger, Post } from '@nestjs/common';
+import { Body, Controller, Get, InternalServerErrorException, Logger, Post } from '@nestjs/common';
 import { Dummy } from 'src/domain/protocols/dummy/dummy.interface';
 import { CreateDummyService } from 'src/domain/services/dummy/create-dummy.service';
+import { GetAllDummyService } from 'src/domain/services/dummy/get-all-dummy.service';
 import { DummyCreateDto } from '../../dto/dummy.dto';
 
 @Controller('dummy')
 export class DummyController {
   constructor(
     private createDummyService: CreateDummyService,
+    private getAllDummyService: GetAllDummyService,
     private logger: Logger
   ) {}
   
@@ -16,6 +18,17 @@ export class DummyController {
       this.logger.log('Creating a new Dummy', 'DummyController.create');
       return await this.createDummyService.create(dummy);
     } catch (error) {
+      throw new InternalServerErrorException('Internal error');
+    }
+  }
+
+  @Get('/')
+  async getAllDummy(): Promise<any> {
+    try {
+      this.logger.log('Starting Get All Dummy', 'DummyController.getAllDummy');
+      return this.getAllDummyService.findAll();
+    } catch (error) {
+      this.logger.error('Error Get All Dummy', error);
       throw new InternalServerErrorException('Internal error');
     }
   }
