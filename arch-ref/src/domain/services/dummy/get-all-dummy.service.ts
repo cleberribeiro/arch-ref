6a1a2fae760a97e45bb3e2947566a9c8b86ca259
Cleber Ribeiro <cleber.ramos.ribeiro@gmail.com>
2021-10-04 22:33:50 -0300

    feat: add service to return all dummies

---
+++ arch-ref/src/domain/services/dummy/get-all-dummy.service.ts
@@ -0,0 +1,17 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { Dummy, IDummyGetAll } from 'src/domain/protocols/dummy/dummy.interface';
+import { DummyRepository } from 'src/infrastructure/repository/dummy.repository';
+
+@Injectable()
+export class GetAllDummyService implements IDummyGetAll {
+
+  constructor(
+    private dummyRepository: DummyRepository,
+    private logger: Logger
+  ) {}
+
+  public async findAll(): Promise<any> {
+    this.logger.log('Call service', 'GetAllDummyService.getAll');
+    return this.dummyRepository.findAll();
+  }
+}
