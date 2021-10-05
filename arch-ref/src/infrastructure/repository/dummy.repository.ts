0763d50a43abc29a1f6f488169d36bb7c59e5b64
Cleber Ribeiro <cleber.ramos.ribeiro@gmail.com>
2021-10-04 22:34:09 -0300

    chore: add methos findAll

--- arch-ref/src/infrastructure/repository/dummy.repository.ts
+++ arch-ref/src/infrastructure/repository/dummy.repository.ts
@@ -1,16 +1,24 @@
+import { Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { DummyEntity } from 'src/domain/entities/dummy/dummy.entity';
 import { Dummy, DummyCreate } from 'src/domain/protocols/dummy/dummy.interface';
 import { MongoRepository } from 'typeorm';
+import { databaseMapper } from './mappers/database.mapper';
 
 export class DummyRepository {
   constructor(
     @InjectRepository(DummyEntity)
     private readonly dummyRepository: MongoRepository<DummyEntity>,
+    private logger: Logger
   ) {}
 
   public async save(data: DummyCreate): Promise<Dummy> {
     return await this.dummyRepository.save(new DummyEntity(data));
+  }
+
+  public async findAll(): Promise<Dummy[]> {
+    this.logger.log('Call DummyRepository', 'DummyRepository.getAll');
+    return this.dummyRepository.find();
   }
 
 }
