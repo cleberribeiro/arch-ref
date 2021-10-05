c32d3acedce24873c396349eb25861be04caa205
Cleber Ribeiro <cleber.ramos.ribeiro@gmail.com>
2021-10-04 22:35:07 -0300

    chore: add provider GetAllDummy

--- arch-ref/src/modules/database.module.ts
+++ arch-ref/src/modules/database.module.ts
@@ -2,6 +2,7 @@ import { Logger, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DummyEntity } from 'src/domain/entities/dummy/dummy.entity';
 import { CreateDummyService } from 'src/domain/services/dummy/create-dummy.service';
+import { GetAllDummyService } from 'src/domain/services/dummy/get-all-dummy.service';
 import { typeormConfig } from 'src/infrastructure/configs/typeormconfig';
 import { DummyRepository } from 'src/infrastructure/repository/dummy.repository';
 import { DummyController } from 'src/interface/http/controllers/dummy/dummy.controller';
@@ -14,6 +15,7 @@ import { DummyController } from 'src/interface/http/controllers/dummy/dummy.cont
     controllers: [DummyController],
     providers: [
       CreateDummyService,
+      GetAllDummyService,
       DummyRepository,
       Logger
     ],
