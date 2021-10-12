260ec75f6e38c917409a4add9c7c1414fbcad0df
Cleber Ribeiro <cleber.ramos.ribeiro@gmail.com>
2021-10-12 15:41:54 -0300

    feat: add update and getOne controllers

--- arch-ref/src/interface/http/controllers/dummy/dummy.controller.ts
+++ arch-ref/src/interface/http/controllers/dummy/dummy.controller.ts
@@ -1,7 +1,11 @@
-import { Body, Controller, Get, InternalServerErrorException, Logger, Post } from '@nestjs/common';
-import { Dummy } from 'src/domain/protocols/dummy/dummy.interface';
+import { Body, Controller, Get, InternalServerErrorException, Logger, Param, Post, Put } from '@nestjs/common';
+import { Dummy, DummyUpdate } from 'src/domain/protocols/dummy/dummy.interface';
 import { CreateDummyService } from 'src/domain/services/dummy/create-dummy.service';
 import { GetAllDummyService } from 'src/domain/services/dummy/get-all-dummy.service';
+import { GetOneDummyService } from 'src/domain/services/dummy/get-one-dummy.service';
+import { UpdateDummyService } from 'src/domain/services/dummy/update-dummy.service';
+import { ParseObjectIdPipe } from 'src/domain/validators/pipe/parse-object-id.pipe';
+import { ObjectID } from 'typeorm';
 import { DummyCreateDto } from '../../dto/dummy.dto';
 
 @Controller('dummy')
@@ -9,6 +13,8 @@ export class DummyController {
   constructor(
     private createDummyService: CreateDummyService,
     private getAllDummyService: GetAllDummyService,
+    private getOneDummyService: GetOneDummyService,
+    private updateDummyService: UpdateDummyService,
     private logger: Logger
   ) {}
   
@@ -29,6 +35,28 @@ export class DummyController {
       return this.getAllDummyService.findAll();
     } catch (error) {
       this.logger.error('Error Get All Dummy', error);
+      throw new InternalServerErrorException('Internal error');
+    }
+  }
+
+  @Get('/:id')
+  async findById(@Param('id') id: ObjectID): Promise<Dummy> {
+    try {
+      this.logger.log('Starting Get All Dummy', 'DummyController.getAllDummy');
+      return await this.getOneDummyService.findById(id);
+    } catch (error) {
+      this.logger.error('Error Get All Dummy', error);
+      throw new InternalServerErrorException('Internal error');
+    }
+  }
+
+  @Put('/:id')
+  async update(@Param('id') id: ObjectID, @Body() data: DummyUpdate): Promise<Dummy> {
+    try {
+      this.logger.log('Starting Update Dummy', 'DummyController.update');
+      return await this.updateDummyService.update(id, data);
+    } catch (error) {
+      this.logger.error('Error Update Dummy', error);
       throw new InternalServerErrorException('Internal error');
     }
   }
