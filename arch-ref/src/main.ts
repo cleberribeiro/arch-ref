b40d6ace2bbd4ee9e87df5ae33e0058ef27e9f1d
Cleber Ribeiro <cleber.ramos.ribeiro@gmail.com>
2021-10-12 18:06:47 -0300

    feat: add helmet

--- arch-ref/src/main.ts
+++ arch-ref/src/main.ts
@@ -2,10 +2,12 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.use(helmet());
 
   const config = new DocumentBuilder()
     .setTitle('Arch-Ref')
