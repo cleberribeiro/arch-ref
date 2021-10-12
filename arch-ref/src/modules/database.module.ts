af87886eae71a2cfb4ff60d6a8345426db10274c
Cleber Ribeiro <cleber.ramos.ribeiro@gmail.com>
2021-10-12 15:42:06 -0300

    feat: add update and getOne services

--- arch-ref/src/modules/database.module.ts
+++ arch-ref/src/modules/database.module.ts
@@ -3,6 +3,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DummyEntity } from 'src/domain/entities/dummy/dummy.entity';
 import { CreateDummyService } from 'src/domain/services/dummy/create-dummy.service';
 import { GetAllDummyService } from 'src/domain/services/dummy/get-all-dummy.service';
+import { GetOneDummyService } from 'src/domain/services/dummy/get-one-dummy.service';
+import { UpdateDummyService } from 'src/domain/services/dummy/update-dummy.service';
 import { typeormConfig } from 'src/infrastructure/configs/typeormconfig';
 import { DummyRepository } from 'src/infrastructure/repository/dummy.repository';
 import { DummyController } from 'src/interface/http/controllers/dummy/dummy.controller';
@@ -16,6 +18,8 @@ import { DummyController } from 'src/interface/http/controllers/dummy/dummy.cont
     providers: [
       CreateDummyService,
       GetAllDummyService,
+      GetOneDummyService,
+      UpdateDummyService,
       DummyRepository,
       Logger
     ],
