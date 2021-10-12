d7236e95c7415e4d8871d5a11ec916549c095354
Cleber Ribeiro <cleber.ramos.ribeiro@gmail.com>
2021-10-12 15:39:35 -0300

    feat: add getOne interface

--- arch-ref/src/domain/protocols/dummy/dummy.interface.ts
+++ arch-ref/src/domain/protocols/dummy/dummy.interface.ts
@@ -1,9 +1,13 @@
 import { ObjectID } from "typeorm";
 export interface Dummy {
-  id: ObjectID;
+  id: ObjectID | string;
   description: string;
 }
 export interface DummyCreate {
+  description: string;
+}
+
+export interface DummyUpdate {
   description: string;
 }
 export interface IDummyCreate {
@@ -12,4 +16,16 @@ export interface IDummyCreate {
 
 export interface IDummyGetAll {
   findAll(): Promise<Dummy[]>;
+}
+
+export interface IDummyGetOne {
+  findById(id: ObjectID): Promise<Dummy>;
+}
+
+export interface IDummyUpdate {
+  update(id: ObjectID, data: DummyUpdate): Promise<any>;
+}
+
+export interface IDummyRemove {
+  remove(id: ObjectID): Promise<any>;
 }
