ff792ab95c5acc74db543981de5f6322c4aedb15
Cleber Ribeiro <cleber.ramos.ribeiro@gmail.com>
2021-10-13 07:22:35 -0300

    chore: add subscribemodule

--- arch-ref/src/app.module.ts
+++ arch-ref/src/app.module.ts
@@ -2,10 +2,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './modules/database.module';
+import { SubscribersModule } from './modules/subscribes.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
     DatabaseModule,
+    SubscribersModule
   ],
   controllers: [],
   providers: [],
