97ee77cd7f664c7a7567bbd9aa1f9c8b906cd2b5
Cleber Ribeiro <cleber.ramos.ribeiro@gmail.com>
2021-10-13 07:24:10 -0300

    refactor: remove conf rabbitmq

--- arch-ref/src/main.ts
+++ arch-ref/src/main.ts
@@ -3,9 +3,13 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import * as helmet from 'helmet';
+import { ConfigService } from '@nestjs/config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
   app.useGlobalPipes(new ValidationPipe());
   app.use(helmet());
 
