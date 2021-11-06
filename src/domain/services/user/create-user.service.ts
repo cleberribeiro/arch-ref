4ca9ce7758a19061c9ed6a9897aae8ccb02272e1
Cleber Ribeiro <cleber.ramos.ribeiro@gmail.com>
2021-11-06 16:36:40 -0300

    chore: add method hash to encript password and save cahe user created

--- src/domain/services/user/create-user.service.ts
+++ src/domain/services/user/create-user.service.ts
@@ -2,20 +2,25 @@ import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
 import { Cache } from 'cache-manager';
 import { User, UserCreate, IUserCreate } from 'src/domain/protocols/user/user.interface';
 import { UserRepository } from 'src/infrastructure/repository/user.repository';
+import { BcryptService } from 'src/utils/bcrypt/bcrypt.service';
 
 @Injectable()
 export class CreateUserService implements IUserCreate {
 
   constructor(
     @Inject(CACHE_MANAGER) private cacheManager: Cache,
-    private userRepository: UserRepository
+    private userRepository: UserRepository,
+    private bcryptService: BcryptService
   ) {}
 
   public async create(data: UserCreate): Promise<User> {
-    const register = await this.userRepository.save(data);
-    // if (register.id) {
-    //   this.cacheManager.set(String(register.id), register.description, { ttl: 3000 });
-    // }
-    return register;
+
+    data.password = await this.bcryptService.hash(data.password);
+
+    const user = await this.userRepository.save(data);
+    if (user.id) {
+      this.cacheManager.set(String(user.id), user);
+    }
+    return user;
   }
 }
