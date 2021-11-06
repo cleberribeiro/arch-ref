7332daea0d33b549105df577cdc8f972c9ba3a31
Cleber Ribeiro <cleber.ramos.ribeiro@gmail.com>
2021-11-06 17:28:54 -0300

    chore: add test service to send message to queue

--- tests/unit/src/domain/services/user/create-user.service.spec.ts
+++ tests/unit/src/domain/services/user/create-user.service.spec.ts
@@ -1,10 +1,10 @@
 import { CACHE_MANAGER } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 import { Test, TestingModule } from '@nestjs/testing';
 import { User, UserCreate } from 'src/domain/protocols/user/user.interface';
 import { CreateUserService } from 'src/domain/services/user/create-user.service';
 import { UserRepository } from 'src/infrastructure/repository/user.repository';
 import { BcryptService } from 'src/utils/bcrypt/bcrypt.service';
-// import { Cache } from 'cache-manager';
 
 describe('Domain :: Services :: User :: CreateUserService', () => {
 
@@ -12,6 +12,7 @@ describe('Domain :: Services :: User :: CreateUserService', () => {
   let userRepository;
   let bcryptService;
   let createUserService;
+  let publisherService;
 
   beforeEach(async () => {
     const moduleRef: TestingModule = await Test.createTestingModule({
@@ -21,6 +22,12 @@ describe('Domain :: Services :: User :: CreateUserService', () => {
           provide: CACHE_MANAGER,
           useValue: {
             set: jest.fn()
+          }
+        },
+        {
+          provide: 'AMQP_SERVICE',
+          useValue: {
+            send: jest.fn()
           }
         },
         {
@@ -39,13 +46,14 @@ describe('Domain :: Services :: User :: CreateUserService', () => {
     }).compile();
 
     cacheManager = await moduleRef.get<Cache>(CACHE_MANAGER);
+    publisherService = await moduleRef.get<ClientProxy>('AMQP_SERVICE');
     userRepository = await moduleRef.get<UserRepository>(UserRepository);
     bcryptService = await moduleRef.get<BcryptService>(BcryptService);
     createUserService = await moduleRef.get<CreateUserService>(CreateUserService);
 
   });
 
-  it('should return new User if success and cache save', async () => {
+  it('should return new User if success and save in cache and send to queue', async () => {
 
     const mockUserRepository: User = {
       id: '123',
@@ -64,11 +72,14 @@ describe('Domain :: Services :: User :: CreateUserService', () => {
     
     jest.spyOn(userRepository, 'save').mockResolvedValue(mockUserRepository);
 
+    jest.spyOn(publisherService, 'send').mockReturnValue({ subscribe: () => {} })
+
     const response = await createUserService.create(user);
 
     expect(response).toEqual(mockUserRepository);
 
     expect(cacheManager.set).toHaveBeenCalledWith('123', mockUserRepository);
+    expect(publisherService.send).toHaveBeenCalledWith({ cmd: 'add-user' }, { data: mockUserRepository });
 
   });
 })
