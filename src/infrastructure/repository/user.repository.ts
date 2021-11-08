47a9d771d25bb395354359edf008e27081b69fc0
Cleber Ribeiro <cleber.ramos.ribeiro@gmail.com>
2021-11-08 20:48:13 -0300

    refactor: rename dummy to user

--- src/infrastructure/repository/user.repository.ts
+++ src/infrastructure/repository/user.repository.ts
@@ -8,32 +8,32 @@ import { ObjectID } from 'mongodb';
 export class UserRepository {
   constructor(
     @InjectRepository(UserEntity)
-    private readonly userRepository: MongoRepository<UserEntity>,
+    private readonly mongoRepository: MongoRepository<UserEntity>,
     private logger: Logger
   ) {}
 
   public async save(data: UserCreate): Promise<User> {
-    return await this.userRepository.save(new UserEntity(data));
+    return await this.mongoRepository.save(new UserEntity(data));
   }
 
   public async findAll(): Promise<User[]> {
     this.logger.log('Call UserRepository', 'UserRepository.getAll');
-    return this.userRepository.find();
+    return this.mongoRepository.find();
   }
 
   public async findById(id: ObjectIDType): Promise<User> {
     this.logger.log('Call UserRepository', 'UserRepository.findById');
-    return await this.userRepository.findOneOrFail({ where: { _id: new ObjectID(id) } });
+    return await this.mongoRepository.findOneOrFail({ where: { _id: new ObjectID(id) } });
   }
 
   public async update(id: ObjectIDType, data: UserUpdate): Promise<any> {
     this.logger.log('Call UserRepository', 'UserRepository.update');
-    return await this.userRepository.updateOne({ _id: new ObjectID(id) }, { $set: data }, { upsert: true } );
+    return await this.mongoRepository.updateOne({ _id: new ObjectID(id) }, { $set: data }, { upsert: true } );
   }
 
   public async remove(id: ObjectIDType): Promise<any> {
     this.logger.log('Call UserRepository', 'UserRepository.remove');
-    return await this.userRepository.findOneAndDelete({ _id: new ObjectID(id) });
+    return await this.mongoRepository.findOneAndDelete({ _id: new ObjectID(id) });
   }
 
 }
