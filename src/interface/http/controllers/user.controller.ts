cd3c2180c6a23607f8743a55a9c2eb867409d1cb
Cleber Ribeiro <cleber.ramos.ribeiro@gmail.com>
2021-11-08 21:59:00 -0300

    chore: add GetOneUserService

--- src/interface/http/controllers/user.controller.ts
+++ src/interface/http/controllers/user.controller.ts
@@ -1,14 +1,17 @@
-import { Body, Controller, Get, InternalServerErrorException, Logger, Post } from '@nestjs/common';
+import { Body, Controller, Get, InternalServerErrorException, Logger, Param, Post } from '@nestjs/common';
 import { User } from 'src/domain/protocols/user/user.interface';
 import { CreateUserService } from 'src/domain/services/user/create-user.service';
 import { UserCreateDto } from '../dto/user.dto';
 import { GetAllUserService } from 'src/domain/services/user/get-all-user.service';
+import { GetOneUserService } from 'src/domain/services/user/get-one-user.service';
+import { ObjectID } from 'typeorm';
 
 @Controller('user')
 export class UserController {
   constructor(
     private createUserService: CreateUserService,
     private getAllUserService: GetAllUserService,
+    private getOneUserService: GetOneUserService,
     private logger: Logger
   ) {}
   
@@ -28,6 +31,17 @@ export class UserController {
     try {
       this.logger.log('Starting Get All User', 'UserController.getAllUser');
       return this.getAllUserService.findAll();
+    } catch (error) {
+      this.logger.error('Error Get All User', error);
+      throw new InternalServerErrorException('Internal error');
+    }
+  }
+
+  @Get('/:id')
+  async findById(@Param('id') id: ObjectID): Promise<User> {
+    try {
+      this.logger.log('Starting Get All User', 'UserController.getAllUser');
+      return await this.getOneUserService.findById(id);
     } catch (error) {
       this.logger.error('Error Get All User', error);
       throw new InternalServerErrorException('Internal error');
